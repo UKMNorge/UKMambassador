c9f98ffd2b3d7d9fa7f8252d5f199727246fb731
Marius Mandal <mariusmandal@gmail.com>
2013-11-14 10:40:43 +0100

    php syntax

--- ukmambassador.php
+++ ukmambassador.php
@@ -89,7 +89,7 @@ function UKMambassador() {
 		$options[] = array('pl_id' => get_option('pl_id'));
 	}
 	
-	if(isset($_GET['delete']) {
+	if(isset($_GET['delete'])){
 		$amb = new ambassador( (int) $_GET['delete'] );
 		$res_del = $amb->delete();
 	}
@@ -98,7 +98,7 @@ function UKMambassador() {
 				   'ambassadorer' => $pl->ambassadorer(),
 				   'monstringer' => $options,
 				   'invites' => $send_status);
-	if(isset($_GET['delete']) {
+	if(isset($_GET['delete'])){
 		$infos['deleted'] = $res_del;
 	}
 	echo TWIG('ambassador_mine.twig.html', $infos , dirname(__FILE__));
